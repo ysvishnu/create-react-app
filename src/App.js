c54ebe715a244105aa13b10bb6ad366d88fe3580
ysvishnu <30285552+ysvishnu@users.noreply.github.com>
2019-07-10 14:42:51 +0530

    updated text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Vishnu Yeni Shetty, do you want to know about AI ML
           </p>
           <a
             className="App-link"
